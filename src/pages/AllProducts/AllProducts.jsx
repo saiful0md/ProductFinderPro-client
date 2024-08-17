27498b944e99cb81f7c3551c0c6b97b26c658c2f
Saiful Islam <saiful islam>
2024-08-17 16:19:08 +0600

    added search functionality

--- src/pages/AllProducts/AllProducts.jsx
+++ src/pages/AllProducts/AllProducts.jsx
@@ -4,19 +4,47 @@ import { FaStar } from "react-icons/fa";
 
 const AllProducts = () => {
     const [allProducts, setAllProducts] = useState([])
-
+    const [page, setPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(1);
+    const [sort, setSort] = useState('')
+    const [search, setSearch] = useState('')
+    console.log(search);
     useEffect(() => {
-        fetch('http://localhost:5000/allProducts')
+        fetch(`http://localhost:5000/allProducts?sort=${sort}&search=${search}`)
             .then(res => res.json())
             .then(data => {
                 setAllProducts(data);
+                setTotalPages(totalPages)
             })
-    }, [])
+    }, [page, search, sort, totalPages])
+    const handleSearch = e => {
+        e.preventDefault()
+        const searchText = e.target.search.value
+        setSearch(searchText)
+    }
     return (
         <div className="max-w-6xl mx-auto my-20">
-           
+            <div className="flex gap-6">
+                {/* <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} placeholder="Search products..." /> */}
+                <form onSubmit={handleSearch}>
+                    <div className="border-2 border-red-700 rounded-lg py-2 px-4" >
+                        <input type="text" name='search' className="outline-none" />
+                        <input type="submit" value="search" className="" />
+                    </div>
+                </form>
+                {/* sort */}
+                <select
+                    className="border-2 border-red-700 py-2 px-4"
+                    value={sort} onChange={(e) => setSort(e.target.value)}
+                >
+                    <option value="">Sort By</option>
+                    <option value="priceAsc">Price: Low to High</option>
+                    <option value="priceDesc">Price: High to Low</option>
+                    <option value="dateDesc">Newest First</option>
+                </select>
+            </div>
             <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-5 p-2">
-                {allProducts.map((product,index) => (
+                {allProducts.map((product, index) => (
                     <div
                         key={index}
                         className="card  bg-base-100 shadow-xl border py-4">
@@ -42,6 +70,11 @@ const AllProducts = () => {
                         </div>
                     </div>
                 ))}
+            </div>
+            <div>
+                <button onClick={() => setPage(page - 1)} disabled={page === 1}>Previous</button>
+                <span>{page} of {totalPages}</span>
+                <button onClick={() => setPage(page + 1)} disabled={page === totalPages}>Next</button>
             </div>
         </div>
     );
